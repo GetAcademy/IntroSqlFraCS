03f5b9fa03f7f87e41e7a3dd8130502601db8815
GET Academy <terje@getacademy.no>
2023-02-28 10:31:25 +0100

    Initial commit

---
+++ IntroSqlFraCS/DatabaseModel/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace IntroSqlFraCS.DatabaseModel
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int? BirthYear { get; set; }
+        public int? PlaceId { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + BirthYear;
+        }
+    }
+}
---
+++ IntroSqlFraCS/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ - ADO.NET 
+ - Dapper
+ - SqlConnection
+ - CRUD-operasjonene mot en enkelt tabell
+ - Pakke inn som en egen Repository-klasse
+ - (vise generelt repository fra Moodle)
+ - Joins og egne databasemodeller 
+
+    Pause til: 10:38
+
+ */
+
+using System.Data.SqlClient;
+using Dapper;
+using IntroSqlFraCS.DatabaseModel;
+
+const string sql = "SELECT Id, FirstName, BirthYear, PlaceId FROM Person WHERE Id = @Id";
+const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Intro2023;Integrated Security=True";
+
+var connection = new SqlConnection(connectionString);
+var people = connection.Query<Person>(sql, new { Id = 6 });
+foreach (var person in people)
+{
+    Console.WriteLine(person);
+}
+
